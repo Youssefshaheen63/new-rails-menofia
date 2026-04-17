4746b78e6c47b92ed4be938960b44f8c4864094a
Youssefshaheen63 <usef.m.shaheen@gmail.com>
2026-04-17 19:56:40 +0200

    feat(user): add  validations and email uniqueness with format check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :dob, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+             format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+            # format: {
+            #   with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+            #   message: "must be a valid email address"
+            # }
 end
