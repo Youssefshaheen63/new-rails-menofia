d753f529f1538a0c1a1fad5055246b0a509ec0c8
Youssefshaheen63 <usef.m.shaheen@gmail.com>
2026-04-17 19:57:14 +0200

    feat(users): implement CRUD actions with form and display email and address

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,55 @@
 class UsersController < ApplicationController
-  # your code goes here
+   before_action :set_user, only: %i[ show edit update destroy ]
+
+  # GET /users
+  def index
+    @users = User.all 
+  end
+
+  # GET /users/:id
+  def show
+  end
+
+  # GET /users/new
+  def new
+     @user = User.new
+  end 
+
+  # Post /users/
+  def create 
+    @user = User.new(name: params[:user][:name], dob: params[:user][:dob],
+    email: params[:user][:email], phone: params[:user][:phone],
+    address: params[:user][:address])
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET /users/:id/edit
+  def edit
+  end
+
+  # Patch /users/:id
+  def update 
+    if @user.update(name: params[:user][:name], dob: params[:user][:dob],
+    email: params[:user][:email], phone: params[:user][:phone],
+    address: params[:user][:address])
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # Delete /users/:id
+  def destroy 
+    @user.destroy
+    redirect_to users_path
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
