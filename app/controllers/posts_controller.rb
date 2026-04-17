b94ff532d064549055d105411d8415d871991f37
Youssefshaheen63 <usef.m.shaheen@gmail.com>
2026-04-17 17:18:14 +0200

    feat: add CRUD in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,46 @@
 class PostsController < ApplicationController
-  # your code goes here
+  
+  # GET /posts
+  def index
+    @posts = Post.all 
+  end
+
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # GET /posts/new
+  def new
+     @post = Post.new
+  end 
+
+  # Post /posts/commit
+  def create 
+    @post = Post.new(title: params[:post][:title], content: params[:post][:content])
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update 
+    @post = Post.find(params[:id])
+    if @post.update(title: params[:post][:title], content: params[:post][:content])
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 end
